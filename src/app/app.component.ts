f81c81d691557a5f3829d630a4a1a2569ee8659c
Gache <erickfrancodelgado@hotmail.com>
2020-05-27 17:37:31 +0200

    pipes sans commentaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { logWarnings } from 'protractor/built/driverProviders';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+nombre     = 'Erick';
+nombre2    = 'eRIck enRIque franco delgado'
+
+arreglo    = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+PI         = Math.PI;
+
+
+porcentaje = 0.234;
+
+
+salario    = 1234.5;
+
+heroe = {
+  nombre    : 'Logan',
+  clave     : 'Wolverine',
+  edad      :  500,
+  dirreccion:{
+  calle     : 'Primera',
+  casa      : '19'
+  }
+
+};
+
+
+valorDePromesa = new Promise( (resolve, reject) => {
+setTimeout(() => resolve('Llego la data'), 3500 );
+})
+
+fecha = new Date();
+
+video = "mOeSfOJrUIk";
+
+activar: boolean = true;
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Importacion de la fecha para colocar la fecha en espagnol
+
+import { LOCALE_ID } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
-  providers: [],
+  providers: [
+    // { provide: LOCALE_ID, useValue: "es" } no sale en la pantalla voy hacer la pregunta
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
